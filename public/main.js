413049232e5a9359c8798e082761a93d60829b6a
Kate Hudson <k88hudson@gmail.com>
2013-08-26 15:30:53 -0400

    gifs

--- public/main.js
+++ public/main.js
@@ -2,11 +2,14 @@ requirejs.config({
   baseUrl: 'bower_components',
   paths: {
     jade: 'jade/runtime',
-    jquery: 'jquery/jquery'
+    jquery: 'jquery/jquery',
+    animatedGif: 'Animated_GIF/src/Animated_GIF',
+    omggif: 'Animated_GIF/src/omggif',
+    neuquant: 'Animated_GIF/src/NeuQuant'
   }
 });
 
-require(['jquery'], function ($) {
+require(['jquery', 'animatedGif'], function ($, AnimatedGif) {
   var $tile = $('.tile');
   var $video = $('video.recorder');
   var $canvas = $('.canvas');
@@ -28,18 +31,53 @@ require(['jquery'], function ($) {
     }
   }
 
+  var width;
+  var height;
+  var frames = [];
+
+  var count = 0;
+  var COUNT = 5;
+  var DELAY = 200;
+  var interval;
+
+  function makeGif() {
+    var ag = new Animated_GIF({ workerPath: '/bower_components/Animated_GIF/src/quantizer.js' });
+    ag.setSize(320, 240);
+    ag.setDelay(10);
+    for (var i=0; i<frames.length; i++) {
+      ag.addFrame(frames[i]);
+    }
+    var animatedImage = document.createElement('img');
+    ag.getBase64GIF(function(image) {
+      animatedImage.src = image;
+      $photo.append(animatedImage);
+    });
+  }
+
+  function snapPicture() {
+    console.log("SNAP");
+    var data;
+    $canvas[0].getContext('2d').drawImage($video[0], 0, 0, width, height);
+    var img = document.createElement('img');
+    img.src = $canvas[0].toDataURL('image/gif');
+    frames.push(img);
+    count++;
+    if ( count === COUNT ) {
+      clearInterval(interval);
+      makeGif();
+    }
+  }
+
   function onClick() {
-    var width = $video.width();
-    var height = $video[0].videoHeight / ($video[0].videoWidth/width);
+    width = $video.width();
+    height = $video[0].videoHeight / ($video[0].videoWidth / width);
 
     $video.attr('width', width);
     $video.attr('height', height);
     $canvas.attr('width', width);
     $canvas.attr('height', height);
 
-    $canvas[0].getContext('2d').drawImage($video[0], 0, 0, width, height);
-    var data = $canvas[0].toDataURL('image/png');
-    $photo.attr('src', data);
+    interval = setInterval(snapPicture, DELAY);
 
     $video.removeAttr('width');
     $video.removeAttr('height');
@@ -53,9 +91,8 @@ require(['jquery'], function ($) {
     $video.click(onClick);
   }
 
-
-
-  navigator.getMedia({ video: true }, onStreamLoaded, onErr);
-
+  navigator.getMedia({
+    video: true
+  }, onStreamLoaded, onErr);
 
 });
